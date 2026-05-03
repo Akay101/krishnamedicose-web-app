f7cbf072ccc51035aff551e2deef97f2845451da
Akshay Chauhan <akay101685@gmail.com>
2026-05-04 00:20:36 +0530

    Updated CORS config

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,48 +1,56 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const path = require('path');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+require("dotenv").config();
 
 const app = express();
 
 // Middlewares
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://krishnamedicose.in",
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 // Routes
-const authRoutes = require('./routes/authRoutes');
-const contentRoutes = require('./routes/contentRoutes');
-const enquiryRoutes = require('./routes/enquiryRoutes');
-const userRoutes = require('./routes/userRoutes');
-const assetRoutes = require('./routes/assetRoutes');
-const offerRoutes = require('./routes/offerRoutes');
-const analyticsRoutes = require('./routes/analyticsRoutes');
+const authRoutes = require("./routes/authRoutes");
+const contentRoutes = require("./routes/contentRoutes");
+const enquiryRoutes = require("./routes/enquiryRoutes");
+const userRoutes = require("./routes/userRoutes");
+const assetRoutes = require("./routes/assetRoutes");
+const offerRoutes = require("./routes/offerRoutes");
+const analyticsRoutes = require("./routes/analyticsRoutes");
 
-app.use('/api/auth', authRoutes);
-app.use('/api/content', contentRoutes);
-app.use('/api/enquiry', enquiryRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/assets', assetRoutes);
-app.use('/api/offers', offerRoutes);
-app.use('/api/analytics', analyticsRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/content", contentRoutes);
+app.use("/api/enquiry", enquiryRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/assets", assetRoutes);
+app.use("/api/offers", offerRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 // Error Handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send({ message: 'Something went wrong!', error: err.message });
+  res
+    .status(500)
+    .send({ message: "Something went wrong!", error: err.message });
 });
 
 module.exports = app;
